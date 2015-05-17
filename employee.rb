983cc3692c3851cafb3c5fbd199096f299d1bb0f
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-17 16:26:20 -0400

    Pass evaluate_employee_review method in employee

--- employee.rb
+++ employee.rb
@@ -7,6 +7,8 @@ class Employee
     @salary = salary
     @reviews = []
     @performance_rating = performance_rating
+    @bad_comments = []
+    @good_comments = []
   end
 
   def give_raise(dollar_amount)
@@ -18,15 +20,19 @@ class Employee
   end
 
   def evaluate_employee_review
-    bad_comments =  @reviews.each {|r| r.scan(/(improve)|(difficult)|(negative)|(less)|(not)|(lack)/)}
-    good_comments = @reviews.each {|r| r.scan(/(good)|())
-      if good_comments.count > bad_comments.count
+    @bad_comments =  @reviews.each {|r|
+      r.scan(/(improve)|(difficult)|(negative)|(less)|(not)|(lack)|(inadequate)|(concern)|(interrupt)|(poor)|(needs)|(unsatisf)|(\s\more\s\w)/)}
+    @good_comments = @reviews.each {|r|
+      r.scan(/(good)|(success)|(pleasure)|(happy)|(great)|(consistent)|(effectiv)|(\s\satisf)|(\s\productiv)|(\s\perfect)|(outstand)|(excellent)|(postive)|(s\proficient)|(strong)/)}
+      if @good_comments.count > @bad_comments.count
         @performance_rating = "Satisfactorily"
       else
         @performance_rating = "Unsatisfactorily"
-
+      end
   end
 
+  def rating_employee_performance
+  end 
   # def work_performance(r)
   #   # @reviews
   #   if r == true
--- employee_review_test.rb
+++ employee_review_test.rb
@@ -103,7 +103,18 @@ class EmployeeReview < Minitest::Test
     lack the same information, and 3) clients are told that certain features are complete when
     they are inadequate.  This communication limitation could be the fault of project management,
     but given that other developers appear to retain more information, this is worth discussing further.")
-    assert zeke.evaluate_employee_review
+    p zeke.evaluate_employee_review
+    yvonne = Employee.new(name: "Yvonne", salary: 20000)
+    yvonne.add_review("Thus far, there have been two concerns over Yvonne's performance, and
+    both have been discussed with her in internal meetings.  First, in some cases, Yvonne takes
+    longer to complete tasks than would normally be expected.  This most commonly manifests during
+    development on existing applications, but can sometimes occur during development on new projects,
+    often during tasks shared with Andrew.  In order to accommodate for these preferences, Yvonne has
+    been putting more time into fewer projects, which has gone well. Second, while in conversation,
+    Yvonne has a tendency to interrupt, talk over others, and increase her volume when in disagreement.
+    In client meetings, she also can dwell on potential issues even if the client or other attendees have
+    clearly ruled the issue out, and can sometimes get off topic.")
+    p yvonne.evaluate_employee_review
   end
 
 end
