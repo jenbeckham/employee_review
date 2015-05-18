5f70180ff5d34cb9dd653751b90f032ec909e7f0
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-17 21:53:35 -0400

    Count bad commits

--- employee.rb
+++ employee.rb
@@ -1,5 +1,5 @@
 class Employee
-  attr_reader :name, :email, :phone, :salary, :reviews, :performance_rating
+  attr_reader :name, :email, :phone, :salary, :reviews, :performance_rating, :bad_comments
   def initialize (name:, email: "", phone: "", salary:)
     @name = name
     @email = email
@@ -8,6 +8,7 @@ class Employee
     @reviews = []
     @performance_rating = performance_rating
     @bad_comments = []
+    # @good_comments = [/(good)|(success)|(pleasure)|(happy)|(great)|(consistent)|(effectiv)|(\s\satisf)|(\s\productiv)|(\s\perfect)|(outstand)|(excellent)|(postive)|(s\proficient)|(strong)/]
     @good_comments = []
   end
 
@@ -16,23 +17,50 @@ class Employee
   end
 
   def add_review(review)
-    @reviews << review
+    @reviews << review.downcase
   end
 
   def evaluate_employee_review
-    @bad_comments =  @reviews.each {|r|
-      r.scan(/(improve)|(difficult)|(negative)|(less)|(not)|(lack)|(inadequate)|(concern)|(interrupt)|(poor)|(needs)|(unsatisf)|(\s\more\s\w)/)}
-    @good_comments = @reviews.each {|r|
-      r.scan(/(good)|(success)|(pleasure)|(happy)|(great)|(consistent)|(effectiv)|(\s\satisf)|(\s\productiv)|(\s\perfect)|(outstand)|(excellent)|(postive)|(s\proficient)|(strong)/)}
-      if @good_comments.count > @bad_comments.count
-        @performance_rating = "Satisfactorily"
-      else
-        @performance_rating = "Unsatisfactorily"
-      end
+    @bad_comments = bad_comments
+    @reviews.each do |r|
+    bad_comments << r.scan(/(improve|difficult|negative|less|not|lack|inadequate|concern|interrupt|poor|needs|unsatisf|\s\more\s\w)/).flatten
+    end
+    p bad_comments.flatten.count
+
+    # , /(negative)/, /(less)/, /(not)/, /(lack)/, /(inadequate)/, /(concern)/, /(interrupt)/, /(poor)/, /(needs)/, /(unsatisf)/, /(\s\more\s\w)/)}
+    # @bad_comments << r.scan(/(improve)+(difficult)+(negative)+(less)+(not)+(lack)+(inadequate)+(concern)+(interrupt)+(poor)+(needs)+(unsatisf)+(\s\more\s\w)/)
+
+    # r.scan(/(improve)+(difficult)+(negative)+(less)+(not)+(lack)+(inadequate)+(concern)+(interrupt)+(poor)+(needs)+(unsatisf)+(\s\more\s\w)/)
+    #  {|comment| @good_comments[comment] += 1} # @bad_comments =  @reviews.count(/(improve)(difficult)(negative)(less)(not)(lack)(inadequate)(concern)(interrupt)(poor)(needs)(unsatisf)(\s\more\s\w)/)
+    # @reviews.scan()
+    #   r.scan(/(good)|(success)|(pleasure)|(happy)|(great)|(consistent)|(effectiv)|(\s\satisf)|(\s\productiv)|(\s\perfect)|(outstand)|(excellent)|(postive)|(s\proficient)|(strong)/)}
+    # p @good_comments
+    # bad_comments = @bad_comments
+    # p good_comments
+    # p bad_comments
+      # if @good_comments.count > @bad_comments.count
+      #   @performance_rating = "Satisfactorily"
+      # else
+      #   @performance_rating = "Unsatisfactorily"
+      # end
   end
 
-  def rating_employee_performance
-  end 
+  # def rating_employee_performance
+  #   if @good_comments.count/(@good_comments.count+@bad_comments.count) <= 0.25
+  #     "1"
+  #   elsif @good_comments.count/(@good_comments.count+@bad_comments.count) <= 0.5
+  #     "2"
+  #   elsif @good_comments.count/(@good_comments.count+@bad_comments.count) <= 0.75
+  #     "3"
+  #   else
+  #     "4"
+  #   end
+  # end
+
+  def work_performance_status
+  end
+
+
   # def work_performance(r)
   #   # @reviews
   #   if r == true
--- employee_review_test.rb
+++ employee_review_test.rb
@@ -103,18 +103,31 @@ class EmployeeReview < Minitest::Test
     lack the same information, and 3) clients are told that certain features are complete when
     they are inadequate.  This communication limitation could be the fault of project management,
     but given that other developers appear to retain more information, this is worth discussing further.")
-    p zeke.evaluate_employee_review
-    yvonne = Employee.new(name: "Yvonne", salary: 20000)
-    yvonne.add_review("Thus far, there have been two concerns over Yvonne's performance, and
-    both have been discussed with her in internal meetings.  First, in some cases, Yvonne takes
-    longer to complete tasks than would normally be expected.  This most commonly manifests during
-    development on existing applications, but can sometimes occur during development on new projects,
-    often during tasks shared with Andrew.  In order to accommodate for these preferences, Yvonne has
-    been putting more time into fewer projects, which has gone well. Second, while in conversation,
-    Yvonne has a tendency to interrupt, talk over others, and increase her volume when in disagreement.
-    In client meetings, she also can dwell on potential issues even if the client or other attendees have
-    clearly ruled the issue out, and can sometimes get off topic.")
-    p yvonne.evaluate_employee_review
+    p zeke.reviews
+    zeke.evaluate_employee_review
+    # # p zeke.rating_employee_performance
+    # yvonne = Employee.new(name: "Yvonne", salary: 20000)
+    # yvonne.add_review("Thus far, there have been two concerns over Yvonne's performance, and
+    # both have been discussed with her in internal meetings.  First, in some cases, Yvonne takes
+    # longer to complete tasks than would normally be expected.  This most commonly manifests during
+    # development on existing applications, but can sometimes occur during development on new projects,
+    # often during tasks shared with Andrew.  In order to accommodate for these preferences, Yvonne has
+    # been putting more time into fewer projects, which has gone well. Second, while in conversation,
+    # Yvonne has a tendency to interrupt, talk over others, and increase her volume when in disagreement.
+    # In client meetings, she also can dwell on potential issues even if the client or other attendees have
+    # clearly ruled the issue out, and can sometimes get off topic.")
+    # p yvonne.evaluate_employee_review
+    # # p yvonne.rating_employee_performance
+    # xavier = Employee.new(name: "Xavier", salary: 25000)
+    # xavier.add_review("Xavier is a huge asset to SciMed and is a pleasure to work with. He quickly knocks
+    # out tasks assigned to him, implements code that rarely needs to be revisited, and is always willing to
+    # help others despite his heavy workload.  When Xavier leaves on vacation, everyone wishes he didn't have
+    # to go. Last year, the only concerns with Xavier performance were around ownership.  In the past twelve months,
+    # he has successfully taken full ownership of both Acme and Bricks, Inc.  Aside from some false starts with estimates
+    # on Acme, clients are happy with his work and responsiveness, which is everything that his managers could ask for.")
+    # p xavier.evaluate_employee_review
+    # # p xavier.rating_employee_performance
+
   end
 
 end
